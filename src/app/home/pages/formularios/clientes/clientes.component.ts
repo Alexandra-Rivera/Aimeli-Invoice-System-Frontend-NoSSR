13647b7c66341401239ee1fccfd41ffd8ed6aadc
JulietaMaga <julietapatyhnz@gmail.com>
2025-04-02 19:39:29 -0600

    Changes: Creacion de scroll, añadiendo botones, mejorando el estilo, display de elementos

--- src/app/home/pages/formularios/clientes/clientes.component.ts
+++ src/app/home/pages/formularios/clientes/clientes.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { BotonIconoEliminarComponent } from "../../../../components/boton-icono-eliminar/boton-icono-eliminar.component";
+import { BotonRedondeadoComponent } from "../../../../components/boton-redondeado/boton-redondeado.component";
 
 @Component({
   selector: 'app-clientes',
-  imports: [],
+  imports: [BotonIconoEliminarComponent, BotonRedondeadoComponent],
   templateUrl: './clientes.component.html',
   styleUrl: './clientes.component.css'
 })
