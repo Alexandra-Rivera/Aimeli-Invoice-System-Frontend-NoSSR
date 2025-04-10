f2e53cf4d80ec98fc101ac03afbf9d85f26637c7
Alexandra-Rivera-Angel <Alexandra.rivera1102@gmail.com>
2025-04-10 17:29:58 -0600

    :construction: add: se agrega componente de producto para inventario

---
+++ src/app/home/pages/inventario/components/producto-component/producto-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-producto-component',
+  imports: [],
+  templateUrl: './producto-component.component.html',
+  styleUrl: './producto-component.component.css'
+})
+export class ProductoComponentComponent {
+@Input() nombre:string = "Nombre por defecto";
+@Input() descripcion: string = "Descripcion del producto por defecto";
+@Input() cantidad_existencias: number = 5;
+@Input() costo_unitario: number = 5.3;
+@Input() precio_venta: number = 10.50;
+}
