58b978f6e50b7373107c535440ee87cb29199185
Alexandra-Rivera-Angel <Alexandra.rivera1102@gmail.com>
2025-04-12 22:52:39 -0600

    Add: Se agrega campo SKU dentro de componente producto

--- src/app/home/pages/inventario/components/producto-component/producto-component.component.ts
+++ src/app/home/pages/inventario/components/producto-component/producto-component.component.ts
@@ -7,9 +7,10 @@ import { Component, Input } from '@angular/core';
   styleUrl: './producto-component.component.css'
 })
 export class ProductoComponentComponent {
-@Input() nombre:string = "Nombre por defecto";
-@Input() descripcion: string = "Descripcion del producto por defecto";
-@Input() cantidad_existencias: number = 5;
-@Input() costo_unitario: number = 5.3;
-@Input() precio_venta: number = 10.50;
+  @Input() codigo_SKU: string = "XUJKLMNPT001";
+  @Input() nombre:string = "Nombre por defecto";
+  @Input() descripcion: string = "Descripcion del producto por defecto";
+  @Input() cantidad_existencias: number = 5;
+  @Input() costo_unitario: number = 5.3;
+  @Input() precio_venta: number = 10.50;
 }
