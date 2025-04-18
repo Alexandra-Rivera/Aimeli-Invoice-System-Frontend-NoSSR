591c8cb1b0af7aeddfcf0bc9f379e3148b35632d
Alexandra-Rivera-Angel <Alexandra.rivera1102@gmail.com>
2025-04-18 15:05:45 -0600

    Add: Se agrega componente producto y listado-productos para la sección de Ventas

---
+++ src/app/home/pages/movimientos/ventas/listado-productos/listado-productos.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavComponentVentasComponent } from "../components/nav-component-ventas/nav-component-ventas.component";
+import { ProductoComponent } from "../components/producto/producto.component";
+
+@Component({
+  selector: 'app-listado-productos',
+  imports: [NavComponentVentasComponent, ProductoComponent],
+  templateUrl: './listado-productos.component.html',
+  styleUrl: './listado-productos.component.css'
+})
+export class ListadoProductosComponent {
+
+}
