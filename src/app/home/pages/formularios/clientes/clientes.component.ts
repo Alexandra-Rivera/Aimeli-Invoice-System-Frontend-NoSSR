9d2282bf149eadf8bad6abb8007e24021757a513
JulietaMaga <julietapatyhnz@gmail.com>
2025-04-22 21:57:12 -0600

    chages: Cambio en los botones

--- src/app/home/pages/formularios/clientes/clientes.component.ts
+++ src/app/home/pages/formularios/clientes/clientes.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
-import { BotonIconoEliminarComponent } from "../../../../components/boton-icono-eliminar/boton-icono-eliminar.component";
-import { BotonRedondeadoComponent } from "../../../../components/boton-redondeado/boton-redondeado.component";
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-clientes',
-  imports: [BotonIconoEliminarComponent, BotonRedondeadoComponent],
+  imports: [],
   templateUrl: './clientes.component.html',
   styleUrl: './clientes.component.css'
 })
 export class ClientesComponent {
 
+  @Input() clientes: any[] = [];
+
+  
 }
