855d567d2c6cf3ce1385b3b32dfba10f91bb5c13
Alexandra-Rivera-Angel <Alexandra.rivera1102@gmail.com>
2025-04-25 22:27:20 -0600

    changes: Agregando Navegacion entre componentes de compras

--- src/app/home/pages/movimientos/compras/registro-compras/registro-compras.component.ts
+++ src/app/home/pages/movimientos/compras/registro-compras/registro-compras.component.ts
@@ -2,10 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { NavComponentComponent } from "../../../../../components/nav-component/nav-component.component";
 import { RegistroDeComprasTablaComponent } from "./components/registro-de-compras-tabla/registro-de-compras-tabla.component";
 import { initFlowbite } from 'flowbite';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-registro-compras',
-  imports: [NavComponentComponent, RegistroDeComprasTablaComponent],
+  imports: [NavComponentComponent, RegistroDeComprasTablaComponent, RouterLink, RouterLinkActive],
   templateUrl: './registro-compras.component.html',
   styleUrl: './registro-compras.component.css'
 })
