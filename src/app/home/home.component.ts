3ce5a3e10ed5ead604f37227571faac5538022b6
Alexandra-Rivera-Angel <Alexandra.rivera1102@gmail.com>
2025-04-25 21:40:31 -0600

    Changes: Agregando navegación en componente Home hacia el resto de componentes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { initFlowbite } from 'flowbite'; // Import Flowbite initialization function
 
 @Component({
   selector: 'app-home',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
