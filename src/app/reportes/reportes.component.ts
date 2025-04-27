ddbe214079e2aa441e4b5c0e8bbff4d8a638a54e
Alexandra-Rivera <00162121@uca.edu.sv>
2025-04-27 00:07:01 -0600

    add: creacion de pantalla reporte

--- src/app/reportes/reportes.component.ts
+++ src/app/reportes/reportes.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite'; // Import Flowbite initialization function
+import { NavComponentComponent } from '../components/nav-component/nav-component.component';
 
 @Component({
   selector: 'app-reportes',
-  imports: [],
+  imports: [NavComponentComponent],
   templateUrl: './reportes.component.html',
   styleUrl: './reportes.component.css'
 })
-export class ReportesComponent {
+export class ReportesComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+      initFlowbite();
+    }
 
 }
