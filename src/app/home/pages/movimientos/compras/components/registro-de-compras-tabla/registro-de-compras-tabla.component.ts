07dd3fad5af201b47e10ef38029414a5a80b34c5
Alexandra-Rivera-Angel <Alexandra.rivera1102@gmail.com>
2025-06-10 17:47:38 -0600

    changes: Modificaciones en tabla de registro de compras

--- src/app/home/pages/movimientos/compras/components/registro-de-compras-tabla/registro-de-compras-tabla.component.ts
+++ src/app/home/pages/movimientos/compras/components/registro-de-compras-tabla/registro-de-compras-tabla.component.ts
@@ -1,16 +1,20 @@
 import { Component, Input } from '@angular/core';
+import { ProductoInfoCompraproducto } from '../../../../../../shared/interfaces/compraproducto/producto-info-compraproducto';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-registro-de-compras-tabla',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './registro-de-compras-tabla.component.html',
   styleUrl: './registro-de-compras-tabla.component.css'
 })
 export class RegistroDeComprasTablaComponent {
-  @Input() fecha_compra: string = "05 / 03 / 2025";
-  @Input() n_factura: string = "FAC0001";
-  @Input() proveedor: string = "SHEIN";
-  @Input() productos: string = "CAMISOLA BLANCA TIPO CROPTOP, ARITOS...";
-  @Input() metodo_pago : string = "Efectivo";
-  @Input() total: number = 22.50;
+  // @Input() fecha_compra: string = "05 / 03 / 2025";
+  // @Input() n_factura: string = "FAC0001";
+  // @Input() proveedor: string = "SHEIN";
+  // @Input() productos: string = "CAMISOLA BLANCA TIPO CROPTOP, ARITOS...";
+  // @Input() metodo_pago : string = "Efectivo";
+  // @Input() total: number = 22.50;
+
+  @Input() registros!: ProductoInfoCompraproducto[];
 }
